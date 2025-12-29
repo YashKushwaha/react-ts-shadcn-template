77ffa9dc691f44e70af7a0c780fcc0e6850f6eb7
Yash Kushwaha <37941798+YashKushwaha@users.noreply.github.com>
2025-12-29 14:43:44 +0530

    Tailwind css is now working

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import './App.css'
 function App() {
 
   return (
-      <div>PROJECT CREATED </div>
+      <div className="border-4 border-dashed border-red-500 p-6 text-2xl font-bold bg-yellow-100">
+        PROJECT CREATED
+      </div>
   )
 }
 

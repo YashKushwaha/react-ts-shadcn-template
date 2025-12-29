f5ea8583544275ee8a34bfe78faf0cfd7e2d72c4
Yash Kushwaha <37941798+YashKushwaha@users.noreply.github.com>
2025-12-29 15:27:45 +0530

    sample app with shadcn input and button component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { useState } from 'react'
+import { Button} from './components/ui/button'
+import { Input } from './components/ui/input'
 
 function App() {
-
+  const [count, setCount] = useState(0)
   return (
-      <div className="border-4 border-dashed border-red-500 p-6 text-2xl font-bold bg-yellow-100">
-        PROJECT CREATED
+      <div className="border-4 border-solid border-red-500 p-6 text-2xl font-bold bg-grey-100">
+        <Input value={count} 
+                onChange={e => e.target.value}/>
+        <Button onClick={() => setCount(count + 1) }>+</Button>
+        <Button onClick={() => setCount(count - 1) }>-</Button>
       </div>
   )
 }
